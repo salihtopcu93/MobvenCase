16e193a708da8e097cdb1964fda262c21e52c1d4
salih topcu <salih.topcu@kgteknoloji.com>
2019-04-15 13:56:11 +0300

    creates SearchViewModel class

---
+++ MobvenCase/ViewModel/SearchViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SearchViewModel.swift
+//  MobvenCase
+//
+//  Created by salih topcu on 15.04.2019.
+//  Copyright © 2019 salih topcu. All rights reserved.
+//
+
+import Foundation
+
+class SearchViewModel {
+    
+}
