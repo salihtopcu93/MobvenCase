ec853056088bb2a8fdea9dac91bfedb2b4bb0438
salih topcu <salih.topcu@kgteknoloji.com>
2019-04-15 16:37:32 +0300

    configures search screen

--- MobvenCase/Controller/SearchViewController.swift
+++ MobvenCase/Controller/SearchViewController.swift
@@ -10,18 +10,82 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
-    var viewModel : SearchViewModel!
+    @IBOutlet weak var movieNameTextField: UITextField!
+    @IBOutlet weak var yearTextField: UITextField!
+    @IBOutlet weak var typeTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var pagesTextField: UITextField!
+    @IBOutlet weak var doneButtonOutlet: UIButton!
     
-    func configureModel() {
-        self.viewModel = SearchViewModel.init()
-    }
+    
+    var viewModel = SearchViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        configureModel()
     }
+    @IBAction func searchButton(_ sender: Any) {
+        if (movieNameTextField.text?.isEmpty)! {
+            print("Boş bırakmayınız")
+        }else {
+            viewModel.getMovies(name: movieNameTextField.text!, type: typeTextField.text, year: yearTextField!.text)
+        }
+    }
+    @IBAction func typeTextFieldEditingDidBegin(_ sender: UITextField) {
+        typeTextField.resignFirstResponder()
+        pickerView.isHidden = false
+        doneButtonOutlet.isHidden = false
+        
+    }
+    @IBAction func doneButton(_ sender: Any) {
+        pickerView.isHidden = true
+        doneButtonOutlet.isHidden = true
+    }
+    
+    
 
 
+}
+
+extension SearchViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SearchTableViewCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    
+}
+
+extension SearchViewController : UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return viewModel.pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return viewModel.pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedValue = viewModel.pickerData[row]
+        typeTextField.text = selectedValue
+    }
+    
 }
 
