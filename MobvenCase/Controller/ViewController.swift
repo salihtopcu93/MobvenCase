e969fc1fd94230ea914f85a08798f57372dd1ea1
salih topcu <salih.topcu@kgteknoloji.com>
2019-04-15 14:53:00 +0300

    configures SearchViewModel

--- MobvenCase/Controller/ViewController.swift
+++ MobvenCase/Controller/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var viewModel : SearchViewModel!
+    
+    func configureModel() {
+        self.viewModel = SearchViewModel.init()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        configureModel()
     }
 
 
--- MobvenCase/ViewModel/SearchViewModel.swift
+++ MobvenCase/ViewModel/SearchViewModel.swift
@@ -7,7 +7,43 @@
 //
 
 import Foundation
+import Alamofire
+
+protocol SearchViewDelegate: class {
+    func updatedList()
+}
 
 class SearchViewModel {
+    var result : Movie?
+    var delegate: SearchViewDelegate?
     
-}
+    let urlString: String = "http://www.omdbapi.com/?s=Batman&page=2&apikey=6dc0afb6"
+    
+    init() {
+        getMovies()
+    }
+    func getMovies(){
+        setLoading(true)
+        
+        Alamofire.request( urlString).responseJSON
+            { (response) in
+                guard let data = response.data else {return}
+                
+                do {
+                    let result = try JSONDecoder().decode(Movie.self, from: data)
+                    
+                    self.result = result
+                    self.delegate?.updatedList()
+                    
+                } catch let error {
+                    print(error)
+                }
+                self.setLoading(false)
+        }
+    }
+    
+    public func setLoading (_  isLoading : Bool) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = isLoading
+    }
+
+    }
