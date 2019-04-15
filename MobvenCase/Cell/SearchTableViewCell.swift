592bc703f1ba94eeb63d683ac499420c1fb2efe1
salih topcu <salih.topcu@kgteknoloji.com>
2019-04-15 15:12:30 +0300

    creates SearchTableViewCell class

---
+++ MobvenCase/Cell/SearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchTableViewCell.swift
+//  MobvenCase
+//
+//  Created by salih topcu on 15.04.2019.
+//  Copyright © 2019 salih topcu. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
